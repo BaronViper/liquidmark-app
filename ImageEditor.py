0ea7b188afa4fdd9ff4b08a716687a4dd51703fa
JeremyS-T <jtanrev@gmail.com>
2022-12-21 15:20:26 +0800

    Enable editor window

--- ImageEditor.py
+++ ImageEditor.py
@@ -1,3 +1,19 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+
 class ImageEditor:
-    def editorsetup(self, window):
-        pass
+    def editorsetup(self, window, img):
+        for child in window.winfo_children():
+            child.destroy()
+
+        window.geometry("800x600")
+
+        preview_panel = Label(window, image=img)
+        preview_panel.grid(row=2, column=2)
+        panel = Label(window, image=img)
+
+        # set the image as img
+        panel.image = img
+        panel.pack()
--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from tkinter import *
+from tkinter import filedialog
 from PIL import Image, ImageTk
 from ImageEditor import ImageEditor
 
 window = Tk()
+
 editor = ImageEditor()
 
-window.title("Liquid Mark")
-window.geometry("600x600")
 
+def open_img():
+    # Select the Imagename  from a folder
+    x = openfilename()
+
+    # opens the image
+    img = Image.open(x)
+
+    # resize the image and apply a high-quality down sampling filter
+    img = img.resize((250, 250), Image.Resampling.LANCZOS)
+
+    # PhotoImage class is used to add image to widgets, icons etc
+    img = ImageTk.PhotoImage(img)
+
+    editor.editorsetup(window=window, img=img)
+
+
+def openfilename():
+    # open file dialog box to select image
+    # The dialogue box has a title "Open"
+    filename = filedialog.askopenfilename(title='"pen')
+    return filename
+
+
+window.title("Liquid Mark")
+
+window.resizable(width=True, height=True)
 ico = Image.open('assets/logo-white.png')
 photo = ImageTk.PhotoImage(ico)
 window.wm_iconphoto(False, photo)
@@ -18,13 +44,12 @@ resize_logo = logo.resize((350, 350))
 
 img = ImageTk.PhotoImage(resize_logo)
 label = Label(window, image=img, borderwidth=0)
-label.pack()
+label.grid(row=0, column=0, padx=30)
 
 img_label = Label(window, text="Upload Image Here", bg="black", fg="white")
-img_label.pack()
+img_label.grid(row=1, column=0, pady=20)
 
-file = Button(window, text='Select an Image', command=editor.editorsetup(window=window))
-file.pack()
-
+file = Button(window, text='Select an Image', command=open_img)
+file.grid(row=2, column=0, pady=(0, 10))
 
 window.mainloop()
