ed68de4d9005c0f719d5601927fd23e2ecbd870b
JeremyS-T <jtanrev@gmail.com>
2022-12-23 11:35:44 +0800

    Update editor panel.

--- ImageEditor.py
+++ ImageEditor.py
@@ -8,12 +8,21 @@ class ImageEditor:
         for child in window.winfo_children():
             child.destroy()
 
-        window.geometry("800x600")
+        preview = Frame(window, height=525, width=486)
+        editor = Frame(window, bg="black", height=525, width=324)
+        preview.grid(row=0, column=0, sticky="nsew")
+        editor.grid(row=0, column=1, sticky="nsew")
 
-        preview_panel = Label(window, image=img)
-        preview_panel.grid(row=2, column=2)
-        panel = Label(window, image=img)
+        window.grid_columnconfigure(0, weight=1)
+        window.grid_columnconfigure(1, weight=1)
+        window.grid_rowconfigure(0, weight=1)
+
+        panel = Label(preview, image=img)
 
         # set the image as img
         panel.image = img
-        panel.pack()
+        panel.place(x=243, y=262.5, anchor='center')
+
+        # Editor panel setup
+        watermark_text = Label(editor, text="Upload Watermark", bg='black', fg='white')
+        watermark_text.grid(row=0, column=1)
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def open_img():
     img = Image.open(x)
 
     # resize the image and apply a high-quality down sampling filter
-    img = img.resize((250, 250), Image.Resampling.LANCZOS)
+    img.thumbnail((405, 262), Image.Resampling.LANCZOS)
 
     # PhotoImage class is used to add image to widgets, icons etc
     img = ImageTk.PhotoImage(img)
@@ -32,8 +32,8 @@ def openfilename():
 
 
 window.title("Liquid Mark")
-
-window.resizable(width=True, height=True)
+window.geometry('810x525')
+window.resizable(width=False, height=False)
 ico = Image.open('assets/logo-white.png')
 photo = ImageTk.PhotoImage(ico)
 window.wm_iconphoto(False, photo)
@@ -44,12 +44,14 @@ resize_logo = logo.resize((350, 350))
 
 img = ImageTk.PhotoImage(resize_logo)
 label = Label(window, image=img, borderwidth=0)
-label.grid(row=0, column=0, padx=30)
+label.grid(row=0, column=0, padx=230)
 
-img_label = Label(window, text="Upload Image Here", bg="black", fg="white")
+font_tuple = ("Trajan Pro", 20, "bold")
+font_tuple2 = ("Trajan Pro", 10)
+img_label = Label(window, text="Let's Get Started", bg="black", fg="white", font=font_tuple)
 img_label.grid(row=1, column=0, pady=20)
 
-file = Button(window, text='Select an Image', command=open_img)
-file.grid(row=2, column=0, pady=(0, 10))
+file = Button(window, text='Upload an Image', command=open_img, font=font_tuple2)
+file.grid(row=2, column=0, pady=(0, 50))
 
 window.mainloop()
