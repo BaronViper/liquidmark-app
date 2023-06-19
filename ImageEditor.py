f3ef7c41f2940e17a33878c95e3628b4d606811b
JeremyS-T <jtanrev@gmail.com>
2023-06-19 18:22:33 +0800

    Add ability to display selected image in new editor window.

--- ImageEditor.py
+++ ImageEditor.py
@@ -29,11 +29,23 @@ class ImageEditor:
         editor_window.geometry("1080x540")
         editor_window.configure(bg="black")
 
-        preview_panel = Frame(editor_window, bg="white")
+        preview_panel = Frame(editor_window, bg="red", width="810", height="540")
         preview_panel.pack(side=LEFT)
 
-        editor_panel = Frame(editor_window, bg="blue")
+        im_label = Label(preview_panel, borderwidth=0, justify='center')
+
+        editor_panel = Frame(editor_window, bg="blue", width="540", height="540")
         editor_panel.pack(side=RIGHT)
 
-        test_label = Label(preview_panel, text="LOL")
-        test_label.pack()
+        im = Image.open(self.target_pic_filename)
+        max_height = 480
+        width_ratio = max_height / im.height
+        new_width = int(im.width * width_ratio)
+        resized_image = im.resize((new_width, max_height))
+
+        photo = ImageTk.PhotoImage(resized_image)
+
+        im_label.image = photo  # Store a reference to the PhotoImage object
+        im_label.configure(image=photo)
+        im_label.pack(anchor=CENTER)
+# TODO: Fix Image display to center of preview panel
